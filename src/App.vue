57712d687c478db95b102f5ce61ef9c017415f33
Pagani Walter <paganiwalter@gmail.com>
2022-02-21 19:23:51 -0300

    Running indentation problem in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-	<MenuTop />
-	<div class="container-fluid">
-		<div class="row mt-3">
-			<div class="col-sm-12 col-md-2">
-				<AsideMenu />
-			</div>
-			<div class="col-sm-12 col-md-10">
-				<router-view/>
-			</div>
-		</div>
-	</div>
+    <MenuTop />
+        <div class="container-fluid">
+            <div class="row mt-3">
+                <div class="col-sm-12 col-md-4">
+                    <AsideMenu />
+                </div>
+                <div class="col-sm-12 col-md-8">
+                    <router-view/>
+                </div>
+            </div>
+        </div>
 </template>
 
 <script>
@@ -17,31 +17,31 @@ import MenuTop from './components/MenuTop.vue';
 import AsideMenu from './components/AsideMenu.vue';
 
 export default {
-  components: {
-    MenuTop,
-    AsideMenu
-  }
+    components: {
+        MenuTop,
+        AsideMenu
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+    padding: 30px;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+    font-weight: bold;
+    color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
